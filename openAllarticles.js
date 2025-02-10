dd74326769bedf5e8971074a26d4a42898df0d0b
dianaalesia18 <nicadiana.alesia@gmail.com>
2025-02-10 22:06:54 +0200

    Add files via upload

---
+++ openAllarticles.js
@@ -0,0 +1,9 @@
+function openAllarticles() {
+    window.location.href = 'index.html';
+}
+function openContact() {
+    window.location.href = 'contact.html';
+}
+function openReviews() {
+    window.location.href = 'reviews.html';
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+function openAllarticles() {
+    window.location.href = 'index.html';
+}
+
+function openReviews() {
+    window.location.href = 'reviews.html';
+}
+
+function openContact() {
+    window.location.href = 'contact.html';
+}
+
